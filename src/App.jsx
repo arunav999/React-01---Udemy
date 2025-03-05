47bcd3c7aeaaff3d2f0ba39057eaa5ddc9c4dbe8
Arunav Singh <arunavsingh999@gmail.com>
2025-03-05 10:34:05 +0530

    splitting components by feature and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,15 @@
-import { useState } from "react";
-
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcepts/CoreConcept";
-import { EXAMPLES } from "./data";
 
-import { CORE_CONCEPTS } from "./data";
-import TabButton from "./components/TabButton";
+import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  const handleSelect = (selectedButton) => {
-    if (selectedTopic === selectedButton) {
-      setSelectedTopic(null);
-    } else {
-      setSelectedTopic(selectedButton);
-    }
-  };
-
-  /*
-   Different ways to render
-   let tabContent = <p>Please select a topic</p>
-
-   if(selectedTopic) {
-   tabContent = <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-   }
-   */
-
   return (
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-            {/* <CoreConcept
-              image={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
-          </ul>
-        </section>
-
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              onSelect={() => handleSelect("components")}
-              isSelected={selectedTopic === "components"}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect("jsx")}
-              isSelected={selectedTopic === "jsx"}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect("props")}
-              isSelected={selectedTopic === "props"}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              onSelect={() => handleSelect("state")}
-              isSelected={selectedTopic === "state"}
-            >
-              State
-            </TabButton>
-          </menu>
-          {/* tabContent */}
-
-          {/* 
-          Alternate way
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>}
-           */}
-
-          {!selectedTopic ? (
-            <p>Please select a topic.</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
---
+++ src/components/Examples.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+import TabButton from "./TabButton";
+import { EXAMPLES } from "../data";
+
+const Examples = () => {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  const handleSelect = (selectedButton) => {
+    if (selectedTopic === selectedButton) {
+      setSelectedTopic(null);
+    } else {
+      setSelectedTopic(selectedButton);
+    }
+  };
+
+  /*
+   Different ways to render
+   let tabContent = <p>Please select a topic</p>
+
+   if(selectedTopic) {
+   tabContent = <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+   }
+   */
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          onSelect={() => handleSelect("components")}
+          isSelected={selectedTopic === "components"}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          onSelect={() => handleSelect("jsx")}
+          isSelected={selectedTopic === "jsx"}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          onSelect={() => handleSelect("props")}
+          isSelected={selectedTopic === "props"}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          onSelect={() => handleSelect("state")}
+          isSelected={selectedTopic === "state"}
+        >
+          State
+        </TabButton>
+      </menu>
+      {/* tabContent */}
+
+      {/* 
+          Alternate way
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>}
+           */}
+
+      {!selectedTopic ? (
+        <p>Please select a topic.</p>
+      ) : (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Examples;
